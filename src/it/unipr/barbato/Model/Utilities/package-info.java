3907efd3cb39a96e2e83b4042533c9027c7b1bfb
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-04-23 13:37:19 +0200

    Package description

---
+++ src/it/unipr/barbato/Model/Utilities/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The {@code it.unipr.barbato.Model.Utilities} package contains utility classes used in the application.
+ * 
+ * @author Vincenzo Barbato 345728
+ */
+package it.unipr.barbato.Model.Utilities;
